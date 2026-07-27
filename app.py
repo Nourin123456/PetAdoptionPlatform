5304249af73f02ab3c8015883811dd68c8e1a538
Nourin123456 <naveenanourinkm#gmail.com>
2026-07-27 23:18:01 +0530

    done

--- app.py
+++ app.py
@@ -430,6 +430,9 @@ def logout():
 @app.route('/about')
 def about():
     return render_template("about.html")
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
 if __name__ == "__main__":
     create_table()
     app.run(debug=True)
